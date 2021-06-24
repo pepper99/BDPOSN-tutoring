05abe0adff1578661aa63198ec2e1254b423e022
Poptum Charoennaew <3c14irlmao@gmail.com>
2021-06-24 23:03:04 +0700

    (bitonic_peak.cpp) remove solution

--- day03/bitonic_peak.cpp
+++ day03/bitonic_peak.cpp
@@ -7,31 +7,3 @@
 // OUTPUT : Index of "any" bitonic peak in the array
 //
 // Try it out yourself : https://leetcode.com/problems/find-peak-element/
-
-#include <iostream>
-#include <climits>
-using namespace std;
-
-void solve(int a[], int n, int l, int r) {
-    int mid = (l+r)/2;
-    int leftmid = mid - 1 < 0 ? INT_MIN : a[mid - 1];
-    int rightmid = mid + 1 >= n ? INT_MIN : a[mid + 1];
-
-    if(leftmid < a[mid] && a[mid] > rightmid) {
-        cout << mid;
-        return;
-    }
-    else if(leftmid > a[mid]) solve(a, n, l, mid);
-    else if(rightmid > a[mid]) solve(a, n, mid + 1, r);
-}
-
-main()
-{
-    int n;
-    cin >> n;
-    int a[n];
-    for(int i = 0; i < n; i++) cin >> a[i];
-
-    int l = 0, r = n;
-    solve(a, n, l, r);
-}
